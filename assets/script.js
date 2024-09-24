e1d9add90d02c914d42eab2e97e43e41de047424
Maxence LE CLERC <shadange@hotmail.com>
2024-09-24 09:01:02 +0200

    First commint - Intégration des chevrons

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+let FlecheGauche = document.querySelector(".arrow_left"); 
+
+FlecheGauche.addEventListener("click", function () {
+	console.log("Tu as cliqué sur la flêche de gauche !")
+})
+
+let FlecheDroite = document.querySelector(".arrow_right");
+
+FlecheDroite.addEventListener("click", function () {
+	console.log("Tu as cliqué sur la flêche de droite !")
+})
+
+slides.length;
+	console.log(slides.length)
